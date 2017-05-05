65f560dd05703d926c2bcfeb81778a75bf66170d
NafisSazid <nafissazid48@gmail.com>
2017-05-06 01:37:49 +0600

    Initial Push

--- js/index.js
+++ js/index.js
@@ -41,6 +41,50 @@ $('.tab a').on('click', function (e) {
   $(target).fadeIn(600);
   
 });
+function login(){
+var str;
+	       xhttp = new XMLHttpRequest();
+	var email=document.getElementById("email").value;
+	var password=document.getElementById("password").value;
+	var flag;
+			xhttp.onreadystatechange=function(){
+			console.log(this.readyState+" "+this.status+" "+xhttp.responseText);
+				if(this.readyState==4 && this.status==200){
+					var str=xhttp.responseText;
+					str=str.toString();
+					var str2='s';
+					var str3="m";
+					var str4="f";
+					var n=str2.localeCompare(str);
+					var n2=str3.localeCompare(str);
+					var n3=str4.localeCompare(str);
+					document.getElementById("showError").innerHTML=str;
+					document.getElementById("showError2").innerHTML=str2;
+					//document.getElementById("showError3").innerHTML=n3;
+					if(str==str2)
+					{
+						flag++;
+						document.getElementById("showError3").innerHTML=flag;
+					  window.location="student.php";
+					 }
+					 else if(str.charAt(0)==str3.charAt(0))
+					{
+					  window.location="manager.php";
+					 }
+					 else if(str.charAt(0)==str4.charAt(0))
+					 {
+						document.getElementById("showError").innerHTML="Incorrect email or password";
+					 }
+					 //document.getElementById("showError").innerHTML="Incorrect email or password";
+					// document.getElementById("showError").innerHTML=str;
+				}
+					
+			};			
+			xhttp.open("POST", "login.php", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhttp.send("email="+email+"&password="+password);
+		
+	}
 var myIndex = 0;
 carousel();
 function carousel() {
