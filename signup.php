52ffb0fe6da619d9c63367b07ee828784de0ec5c
NafisSazid <nafissazid48@gmail.com>
2017-05-08 00:17:16 +0600

    changed signup

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <?php
 	  session_start();
 	  if(isset($_POST['signup'])){
-		$conn = mysql_connect('localhost', 'smarthall', 'smarthall');
+		$conn = mysql_connect('localhost', 'root', '');
 		$db   = mysql_select_db('smarthall');
 		$name = $_POST["name"];
 		$password = $_POST["password"];
@@ -44,36 +44,31 @@
 		}
 	  }
 	  
-	function sendVerificationBySwift($email,$name,$id)
+	 function sendVerificationBySwift($email,$name,$id)
 {
     require_once 'lib/swift_required.php';
-
-    $subject = 'Smarthall Signup '; // Give the email a subject
+    $subject = 'Hall Management Signup | Verification'; // Give the email a subject
+    $address="http://csedu.cf/smarthall/registration.html?email".$email."&hash=".$id;
     $body = '
  
 Thanks for signing up!
-Your account has been created, you can login with the following credentials .
+Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.
  
 ------------------------
 Username: '.$name.'
-Email: '.$email.'
-Password: '.$password.'
 ------------------------
  
- ';
-
+Please click this link to activate your account:.
+ '.$address;
         $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
             ->setUsername('smarthall.du@gmail.com')
             ->setPassword('smarthall')
             ->setEncryption('ssl');
-
         $mailer = Swift_Mailer::newInstance($transport);
-
         $message = Swift_Message::newInstance($subject)
-            ->setFrom(array('noreply@smarthall.du@gmail.com' => 'Smarthall'))
+            ->setFrom(array('noreply@smarthall.du.com' => 'smarthall'))
             ->setTo(array($email))
             ->setBody($body);
-
         $result = $mailer->send($message);
 }
 	  
