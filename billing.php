a95d42eea3d859e0cc7ae1e89392eff8d7ee6dde
NafisSazid <nafissazid48@gmail.com>
2017-05-07 15:19:54 +0600

    changed date

--- billing.php
+++ billing.php
@@ -6,6 +6,7 @@
 		if(isset($_SESSION['email'])) {	
 		$email=$_SESSION["email"] ;
 		}
+		date_default_timezone_set("Asia/Dhaka");
 		$date = date("Y-m-d");
 		$ym=substr($date,0,8);
 		$sql="SELECT Cost FROM foodorder where Email='$email' AND date like '".$ym."%';";
